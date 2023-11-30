5fda2c56be3a73cb6dfaf011573cc4a981d4e435
Tianrui Zhang (Gary) <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-11-30 14:13:16 +0800

    init

---
+++ src/main/java/MSACHAT/controller/AuthController.java
@@ -0,0 +1,30 @@
+package MSACHAT.controller;
+
+import lombok.AllArgsConstructor;
+
+import MSACHAT.dto.JWTAuthResponse;
+
+import MSACHAT.dto.LoginDto;
+
+import MSACHAT.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    // Build Login REST API
+    @PostMapping("/login")
+    public ResponseEntity<JWTAuthResponse> authenticate(@RequestBody LoginDto loginDto){
+        String token = authService.login(loginDto);
+
+        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+}
---
+++ src/main/java/MSACHAT/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package MSACHAT.repository;
+
+import MSACHAT.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Map;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
