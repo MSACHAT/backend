44a2a5177f2597b0af07acf5ccdd4d91bedb28e3
Tianrui Zhang (Gary) <tianrui.zhang.2101@stu.moonshotacademy.cn>
2023-12-01 15:39:15 +0800

    delete CommentDto.java and PostRepositoryImpl.java

--- src/main/java/MSACHAT/service/PostService.java
+++ src/main/java/MSACHAT/service/PostService.java
@@ -1,10 +1,4 @@
 package MSACHAT.service;
 
-import MSACHAT.dto.PostDto;
-import MSACHAT.entity.PostEntity;
-
-import java.util.List;
-
 public interface PostService {
-    List<PostEntity> findAll();
 }
