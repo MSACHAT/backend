7efbc75aaa9ead067ce68642ed0191ac52f6ba97
StanH123h <1735443634@qq.com>
2024-01-15 20:13:01 +0800

    Profile route Fix commit Influenced Domains:config,controller

--- src/main/java/MSACHAT/backend/controller/ProfileController.java
+++ src/main/java/MSACHAT/backend/controller/ProfileController.java
@@ -2,16 +2,21 @@ package MSACHAT.backend.controller;
 
 import MSACHAT.backend.dto.ErrorDto;
 import MSACHAT.backend.dto.PostDto;
+import MSACHAT.backend.dto.PostReturnDto;
+import MSACHAT.backend.entity.ImageEntity;
 import MSACHAT.backend.entity.PostEntity;
 import MSACHAT.backend.mapper.Mapper;
 import MSACHAT.backend.repository.PostRepository;
 import MSACHAT.backend.service.AuthService;
 import MSACHAT.backend.service.CommentService;
+import MSACHAT.backend.service.ImageService;
 import MSACHAT.backend.service.PostService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,28 +27,58 @@ public class ProfileController {
     private PostService postService;
     private AuthService authService;
     private CommentService commentService;
-
+    private ImageService imageService;
     private Mapper<PostEntity, PostDto> postMapper;
 
     public ProfileController(
+            ImageService imageService,
             PostService postService,
             AuthService authService,
             CommentService commentService) {
+        this.imageService=imageService;
         this.postService = postService;
         this.authService = authService;
         this.commentService = commentService;
     }
 
     @GetMapping("")
-    public ResponseEntity<PostRepository.PostResponse> getPostsByUserId(
+    public ResponseEntity<Object> getPostsByUserId(
             @RequestHeader("Authorization") String bearerToken,
             @RequestParam(value = "pageNum") Integer pageNum,
             @RequestParam(value = "pageSize") Integer pageSize) {
-        System.out.println("PageNum Param: " + pageNum);
-        System.out.println("PageSize Param: " + pageSize);
-        String token=authService.getTokenFromHeader(bearerToken);
-        Integer userId=authService.getUserIdFromToken(token);
-        return new ResponseEntity<>(postService.getAllPostsByUserId(userId, pageNum, pageSize), HttpStatus.OK);
+        String token = authService.getTokenFromHeader(bearerToken);
+        Integer userId = authService.getUserIdFromToken(token);
+        if (pageSize == null || pageNum == null) {
+            return new ResponseEntity<>(new ErrorDto(
+                    "Request body incomplete. Required fields missing.",
+                    10001),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        Map<String, Object> postResponse = postService.getPostsByUserId(userId, pageNum, pageSize);
+
+        // bug: @data and @entity cannot use together
+        // temporary use dto to avoid exceptions
+        // todo: fix PostRepository
+        List<PostReturnDto> postsReturnDtos = new ArrayList<>();
+
+        for (PostEntity post : (Page<PostEntity>) postResponse.get("posts")) {
+            postsReturnDtos.add(new PostReturnDto(
+                    post.getId(),
+                    post.getUserName(),
+                    post.getContent(),
+                    post.getImages().stream().map(ImageEntity::getImageUrl).toList(),
+                    post.getTimeStamp(),
+                    post.getLikeCount(),
+                    post.getCommentCount(),
+                    post.isLiked(),
+                    imageService.getAvatar(userId)));
+            System.out.println(post.getContent());
+        }
+
+        postResponse.put("posts", postsReturnDtos);
+
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
     @GetMapping("/getbypagenumandpagesize")
